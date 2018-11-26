9fe0c6ae62346926d2173b82ea87b6db9e656a3a
Izabela Borges <izabelac.borges@gmail.com>
2018-11-26 02:06:14 -0200

    Added books data to a general variables file

---
+++ code/variables.py
@@ -0,0 +1,8 @@
+# list of tuples with book names and links
+BOOKS = [("hp1_sorcerers-stone", "http://www.glozman.com/TextPages/Harry%20Potter%201%20-%20Sorcerer's%20Stone.txt"), 
+         ("hp2_chamber-of-secrets", "http://www.glozman.com/TextPages/Harry%20Potter%202%20-%20Chamber%20of%20Secrets.txt"), 
+         ("hp3_prisioner-of-azkaban", "http://www.glozman.com/TextPages/Harry%20Potter%203%20-%20The%20Prisoner%20Of%20Azkaban.txt"), 
+         ("hp4_globet-of-fire", "http://www.glozman.com/TextPages/Harry%20Potter%204%20-%20The%20Goblet%20Of%20Fire.txt"), 
+         ("hp5_order-of-the-phoenix", "http://www.glozman.com/TextPages/Harry%20Potter%205%20-%20Order%20of%20the%20Phoenix.txt"), 
+         ("hp6_half-blood-prince", "http://www.glozman.com/TextPages/Harry%20Potter%206%20-%20The%20Half%20Blood%20Prince.txt"), 
+         ("hp7_deathly-hallows", "http://www.glozman.com/TextPages/Harry%20Potter%207%20-%20Deathly%20Hollows.txt")]
