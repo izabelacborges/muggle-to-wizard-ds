4677e9736c756deec7e05ccd6f9cc1853fc11259
Izabela Borges <izabelac.borges@gmail.com>
2018-12-11 15:43:53 -0200

    Adding pre-course

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,19 @@
-import urllib.request
+from urllib import request
+import requests as r
+import pandas as pd
 
-def download_data(data):
-    for filename, url in data:
-        urllib.request.urlretrieve(url, './data/{}.txt'.format(filename))
+def download_files(data):
+    for filename, url, extension in data:
+        request.urlretrieve(url, '../data/{}.{}'.format(filename, extension))
         print("Downloaded '{}' to 'data/' folder.".format(filename))
+
+def extract_html_table(data, table_header=0, na_values=None):
+    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36", "X-Requested-With": "XMLHttpRequest"}
+    for filename, url, extension in data:
+        webpage = r.get(url, headers=header)
+        df = pd.read_html(webpage.text, header=table_header, na_values=na_values)[0].dropna()
+        out = df.to_json(orient='records', lines=True)
+        
+        with open('../data/{}.{}'.format(filename, extension), 'w') as f:
+            f.write(out)
+            print("Downloaded '{}' to 'data/' folder.".format(filename))
--- code/variables.py
+++ code/variables.py
@@ -1,8 +1,14 @@
 # list of tuples with book names and links
-BOOKS = [("hp1_sorcerers-stone", "http://www.glozman.com/TextPages/Harry%20Potter%201%20-%20Sorcerer's%20Stone.txt"), 
-         ("hp2_chamber-of-secrets", "http://www.glozman.com/TextPages/Harry%20Potter%202%20-%20Chamber%20of%20Secrets.txt"), 
-         ("hp3_prisioner-of-azkaban", "http://www.glozman.com/TextPages/Harry%20Potter%203%20-%20The%20Prisoner%20Of%20Azkaban.txt"), 
-         ("hp4_globet-of-fire", "http://www.glozman.com/TextPages/Harry%20Potter%204%20-%20The%20Goblet%20Of%20Fire.txt"), 
-         ("hp5_order-of-the-phoenix", "http://www.glozman.com/TextPages/Harry%20Potter%205%20-%20Order%20of%20the%20Phoenix.txt"), 
-         ("hp6_half-blood-prince", "http://www.glozman.com/TextPages/Harry%20Potter%206%20-%20The%20Half%20Blood%20Prince.txt"), 
-         ("hp7_deathly-hallows", "http://www.glozman.com/TextPages/Harry%20Potter%207%20-%20Deathly%20Hollows.txt")]
+BOOKS = [("hp1_sorcerers_stone", "http://www.glozman.com/TextPages/Harry%20Potter%201%20-%20Sorcerer's%20Stone.txt", "txt"), 
+         ("hp2_chamber_of_secrets", "http://www.glozman.com/TextPages/Harry%20Potter%202%20-%20Chamber%20of%20Secrets.txt", "txt"), 
+         ("hp3_prisioner_of_azkaban", "http://www.glozman.com/TextPages/Harry%20Potter%203%20-%20The%20Prisoner%20Of%20Azkaban.txt", "txt"), 
+         ("hp4_globet_of_fire", "http://www.glozman.com/TextPages/Harry%20Potter%204%20-%20The%20Goblet%20Of%20Fire.txt", "txt"), 
+         ("hp5_order_of_the_phoenix", "http://www.glozman.com/TextPages/Harry%20Potter%205%20-%20Order%20of%20the%20Phoenix.txt", "txt"), 
+         ("hp6_half_blood_prince", "http://www.glozman.com/TextPages/Harry%20Potter%206%20-%20The%20Half%20Blood%20Prince.txt", "txt"), 
+         ("hp7_deathly_hallows", "http://www.glozman.com/TextPages/Harry%20Potter%207%20-%20Deathly%20Hollows.txt", "txt")]
+
+# list of tuples with adjacent files
+SPELLS = [("hp_spells_list", "https://www.pojo.com/harry-potter-spell-list/", "json")]
+EXTRAS = [("hp_places_list", "http://m.uploadedit.com/bbtc/1544391705882.txt", "csv"),
+          ("hp_characters_list", "http://m.uploadedit.com/bbtc/1544392366399.txt", "csv"),
+          ("hp_classes_list","http://m.uploadedit.com/bbtc/154439335942.txt", "csv")]
